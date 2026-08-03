7ee559773108c556896cece4e648026221a203be
sivasai88867 <sivaseshasai.v@gmail.com>
2026-08-03 16:48:35 +0530

    modified the cobol program to detect it

--- application/cobol/SIVA/test.cbl
+++ application/cobol/SIVA/test.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO-WORLD.
        PROCEDURE DIVISION.
-           DISPLAY 'modified the HELLO WORLD to detect the change-5'.
+           DISPLAY 'modified the HELLO WORLD to detect the change-6'.
            STOP RUN.
