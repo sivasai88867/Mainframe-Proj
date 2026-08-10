e4a9900241336d9ca85484d91eafda2ce1749ca1
sivasai88867 <sivaseshasai.v@gmail.com>
2026-08-10 19:31:00 +0530

    modified test.cbl to copy it to PDS from VS

--- application/cobol/SIVA/test.cbl
+++ application/cobol/SIVA/test.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO-WORLD.
        PROCEDURE DIVISION.
-           DISPLAY 'modified the HELLO WORLD to detect the change-6'.
+           DISPLAY 'modified the HELLO WORLD to copy it to PDS from VS'
            STOP RUN.
